58828ddd51e9e2c0d053387d258504d16cf5c3f7
Linga <lingareddy.eruva@gmail.com>
2017-12-05 15:03:39 +0530

    added 2 files list_practice and odd_even files.

---
+++ python_Practice/practice/lists_practice.py
@@ -0,0 +1,13 @@
+'''
+Created on 05-Dec-2017
+
+@author: Linga
+'''
+a = [1,1,2,3,5,9,13,34,21,12,89]
+l = []
+
+for i in a:
+    if i <= 20:
+        l.append(i) 
+
+print(l)
---
+++ python_Practice/practice/odd_even.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number"))
+
+if (number % 2) == 0:
+    print("number {0} is : Even".format(number))
+else:
+    print("number {0} is : Odd".format(number))
