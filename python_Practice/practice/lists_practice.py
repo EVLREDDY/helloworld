d4b645a5d81aba9c5011d126770ac09125e610a3
Linga <lingareddy.eruva@gmail.com>
2017-12-05 16:47:55 +0530

    xrange method practice

--- python_Practice/practice/lists_practice.py
+++ python_Practice/practice/lists_practice.py
@@ -3,6 +3,7 @@ Created on 05-Dec-2017
 
 @author: Linga
 '''
+from IPython.utils.py3compat import xrange
 a = [1,1,2,3,5,9,13,34,21,12,89]
 l = []
 
@@ -10,5 +11,17 @@ print(isinstance(a,list))
 for i in a:
     if i <= 20:
         l.append(i) 
-
 print(l)
+
+''' 
+range is used to create the list of numbers dynamically
+'''
+user_input = int(input("Enter any number for which you want divisors."))
+divisors = []
+x = xrange(1,101)
+
+for i in x:
+    if (user_input % i) == 0:
+        divisors.append(i)
+
+print(divisors)
