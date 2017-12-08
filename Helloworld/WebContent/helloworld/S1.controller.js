aa09bea3c62f4b36227dfcb6ad51b9cf7bade8da
Linga <lingareddy.eruva@gmail.com>
2017-12-08 10:36:56 +0530

    table added for table header fix logic RD

--- Helloworld/WebContent/helloworld/S1.controller.js
+++ Helloworld/WebContent/helloworld/S1.controller.js
@@ -1,5 +1,10 @@
 sap.ui.controller("helloworld.S1", {
-  onInit:function(){
-	  console.log("Hellow World")
-  },
+	onInit : function() {
+		var obj = {name:"linga"};
+		var data = [];
+		for(var i=0;i<20;i++){
+			data.push(obj);
+		}
+		this.byId("tableId").setModel(new sap.ui.model.json.JSONModel(data));
+	},
 });
