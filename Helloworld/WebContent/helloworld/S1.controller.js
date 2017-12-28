479928556656d6229acf4d1a887f7ce6863d3f06
Linga <lingareddy.eruva@gmail.com>
2017-12-28 15:52:08 +0530

    input with suggestions code added
    Tried input suggessions

--- Helloworld/WebContent/helloworld/S1.controller.js
+++ Helloworld/WebContent/helloworld/S1.controller.js
@@ -6,5 +6,22 @@ sap.ui.controller("helloworld.S1", {
 			data.push(obj);
 		}
 		this.byId("tableId").setModel(new sap.ui.model.json.JSONModel(data));
+		this.byId("productInput").setModel(new sap.ui.model.json.JSONModel(this.getData()));
+	},
+	
+	getData:function(){
+		return [{
+			name:"Linga"
+		},{
+			name:"Linga 1"
+		},{
+			name:"Linga 2"
+		},{
+			name:"abdkj ldkf"
+		},{
+			name:"bbbbbb"
+		},{
+			name:"kllliw uyiuyiu"
+		}]
 	},
 });
