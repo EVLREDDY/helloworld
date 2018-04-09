d4afe3e3c4121caad417e381767b47bd1371066f
Linga <lingareddy.eruva@gmail.com>
2018-04-09 23:04:54 +0530

    class used for learning

--- python_Practice/practice/userDetails.py
+++ python_Practice/practice/userDetails.py
@@ -4,3 +4,14 @@ Created on 07-Apr-2018
 @author: Linga
 '''
 
+class userDetails:
+    def __init__(self,name):
+        self.name = name
+    def change_name(self,new_name):
+        self.name = new_name
+
+user_Details = userDetails("Linga")
+print(user_Details.name)
+user_Details.change_name('Ram')
+print(user_Details.name);
+    
